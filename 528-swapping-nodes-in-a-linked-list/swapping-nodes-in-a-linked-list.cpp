2b9e71c8f953aaf6b2018e5fff7d6382dfd10c44
Amitesh Shukla <amiteshsmb@gmail.com>
2025-11-15 10:21:34 +0530

    Time: 0 ms (100.00%) | Memory: 185 MB (93.76%) - LeetSync

--- 528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.cpp
+++ 528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.cpp
@@ -11,23 +11,37 @@
 class Solution {
 public:
     ListNode* swapNodes(ListNode* head, int k) {
-        ListNode* temp  = head;
-        vector<int>v;
-        while(temp){
-            v.push_back(temp->val);
-            temp=temp->next;
+        ListNode* first =head; ListNode* second =head;
+        for(int i=0;i<k-1;i++){
+                second=second->next;
         }
-        int n=v.size();
-        int dum = v[k-1];
-        v[k-1] =v[n-k];
-        v[n-k] = dum;
+        ListNode* pointout=second;
+        while(second->next){
+            first=first->next;
+            second=second->next;
+        }
+        swap(pointout->val, first->val);
+        return head;
 
-        ListNode* temp2 = new ListNode(0);
-        ListNode* start=temp2;
-        for(int it:v){
-            temp2->next =new ListNode(it);
-            temp2=temp2->next;
-        }
-        return start->next;
+
+
+        // ListNode* temp  = head;
+        // vector<int>v;
+        // while(temp){
+        //     v.push_back(temp->val);
+        //     temp=temp->next;
+        // }
+        // int n=v.size();
+        // int dum = v[k-1];
+        // v[k-1] =v[n-k];
+        // v[n-k] = dum;
+
+        // ListNode* temp2 = new ListNode(0);
+        // ListNode* start=temp2;
+        // for(int it:v){
+        //     temp2->next =new ListNode(it);
+        //     temp2=temp2->next;
+        // }
+        // return start->next;
     }
 };
