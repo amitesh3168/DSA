d1899ec0239227c52f6580b9357da8aa19a0ac51
Amitesh Shukla <amiteshsmb@gmail.com>
2025-12-08 10:36:19 +0530

    Time: 22 ms (45.41%) | Memory: 38 MB (48.58%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -1,25 +1,39 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int m = mat.size(), n = mat[0].size(), INF = m + n; 
-        for (int r = 0; r < m; r++) {
-            for (int c = 0; c < n; c++) {
-                if (mat[r][c] == 0) continue;
-                int top = INF, left = INF;
-                if (r - 1 >= 0) top = mat[r - 1][c];
-                if (c - 1 >= 0) left = mat[r][c - 1];
-                mat[r][c] = min(top, left) + 1;
+        int n = mat.size();
+        int m  = mat[0].size();
+        queue<pair<pair<int,int>, int>>q;
+        vector<vector<bool>> visited(n, vector<bool>(m));
+        vector<vector<int>>distance(n,vector<int>(m,0));
+        for(int i=0;i<n;i++){
+            for(int j =0;j<m;j++){
+                if(mat[i][j]==0){
+                    q.push({{i,j},0});
+                    visited[i][j]=1;
+                }
             }
         }
-        for (int r = m - 1; r >= 0; r--) {
-            for (int c = n - 1; c >= 0; c--) {
-                if (mat[r][c] == 0) continue;
-                int bottom = INF, right = INF;
-                if (r + 1 < m) bottom = mat[r + 1][c];
-                if (c + 1 < n) right = mat[r][c + 1];
-                mat[r][c] = min(mat[r][c], min(bottom, right) + 1);
+        int er[] = {1,-1,0,0};
+        int ec[] ={0,0,1,-1};
+        while(!q.empty()){
+            auto [p,d] = q.front();
+            auto [x,y] =p;
+            q.pop();
+            distance[x][y]=d;
+            for(int k=0;k<4;k++){
+                int x1 = x+er[k];
+                int y1 = y + ec[k];
+               if(x1>=0&&y1>=0&&x1<n&&y1<m) {
+                if(!visited[x1][y1]){
+                    visited[x1][y1] =1;
+                    q.push({{x1,y1},d+1});
+
+                }
+                }
             }
+
         }
-        return mat;
+        return distance;
     }
-};                                                                                                        
+};
