699d97de2cf0b009319e46fd228a5a15d393016f
Amitesh Shukla <amiteshsmb@gmail.com>
2025-12-12 12:47:42 +0530

    Time: 10 ms (48.59%) | Memory: 35.6 MB (44.45%) - LeetSync

---
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+void dfs(int x, int  y, vector<vector<int>>& board, vector<vector<bool>>&visited,int er[], int ec[],int m, int n){
+    visited[x][y] = 1;
+    for(int k = 0;k<4;k++){
+    int i = x + er[k];
+    int j  = y  + ec[k];
+    if(i>=0&&j>=0&&i<m&&j<n&&!visited[i][j]){  
+            if(board[i][j]==1){
+           dfs(i,j,board,visited,er,ec,m,n);
+    }
+    }
+}}
+    int numEnclaves(vector<vector<int>>& board) {
+           int m = board.size();
+         int n = board[0].size();
+        
+         vector<vector<bool>>visited(m, vector<bool>(n,0));
+            int er[] ={1,-1,0,0};
+            int ec[] ={0,0, -1,1};
+        for(int i =0;i<m;i++){
+            if(board[i][0]==1) dfs(i,0,board, visited,er,ec, m, n);
+             if(board[i][n-1]==1) dfs(i,n-1,board, visited,er,ec,m,n);
+        }
+        for(int i =0;i<n;i++){
+            if(board[0][i]==1) dfs(0,i,board, visited,er,ec, m,n);
+             if(board[m-1][i]==1) dfs(m-1,i,board, visited,er,ec, m, n);
+        }
+        int count =0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if (!visited[i][j] && board[i][j]==1){
+                    count++;
+                }
+            }
+        }
+return count;
+
+    }
+};
