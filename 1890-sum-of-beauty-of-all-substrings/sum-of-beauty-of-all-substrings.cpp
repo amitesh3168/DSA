88f324c9cf68e1222c64755811774ebf72088c8b
Amitesh Shukla <amiteshsmb@gmail.com>
2026-01-01 00:43:47 +0530

    Time: 110 ms (65.27%) | Memory: 11.2 MB (63.84%) - LeetSync

---
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int beautySum(string s) {
+       int n = s.size();
+       int ans=0;
+       for(int i=0;i<n;i++){
+        vector<int>freq(26,0);
+        for(int j=i;j<n;j++){
+            freq[s[j]-'a']++;
+            int mini=INT_MAX;
+            int maxi=0;
+            for(int k=0;k<26;k++){
+               if(freq[k]>0) {
+                mini = min(mini,freq[k]);
+                maxi = max(maxi,freq[k]);
+                }
+            }
+            ans+=(maxi-mini);
+        }
+       } 
+       return ans;
+    }
+};
