f5ceca62e4abefdc46b1a7f50019f3b703ad6e0f
Amitesh Shukla <amiteshsmb@gmail.com>
2026-01-10 23:35:27 +0530

    Time: 15 ms (33.86%) | Memory: 22 MB (23.28%) - LeetSync

---
+++ 2414-move-pieces-to-obtain-a-string/move-pieces-to-obtain-a-string.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool canChange(string start, string target) {
+    int n = start.size();
+    int i =0, j =0;
+    while(i<n || j<n){
+        while(i<n && start[i]=='_') i++;
+        while(j<n && target[j]=='_') j++;
+
+        if(i==n && j==n) return true;
+
+        if(i==n || j==n) return false;
+
+
+
+
+        if(start[i]!=target[j]){
+            return false;
+        }
+
+        if(start[i]=='L' && i<j) return false;
+        if(start[i]=='R' && j<i) return false;
+
+
+        i++;
+        j++;
+
+    }
+    
+    
+    
+    return 1;
+    }
+};
