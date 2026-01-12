e6f6cfba1fb3ed4867ea86bd821e9a00679e6480
Amitesh Shukla <amiteshsmb@gmail.com>
2026-01-13 02:14:16 +0530

    Time: 0 ms (100.00%) | Memory: 14.5 MB (16.45%) - LeetSync

--- 1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.cpp
+++ 1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
-        int m = mat.size();
+           int m = mat.size();
         int n =mat[0].size();
-      vector<pair<int, int>>uttar;
-      vector<int> vic(k);
-      
-        for(int i=0;i<m;i++){
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> yepqhai;
+             for(int i=0;i<m;i++){
             int maionehu =0 ;
             for(int j=0;j<n;j++){
                 if(mat[i][j]==1){
                     maionehu++;
                 }
             }
-           uttar.push_back({maionehu,i});
+           yepqhai.push({maionehu,i});
+          }
+            vector<int>yhiuttarhai;
+        while(yepqhai.size()> m-k){
+            yhiuttarhai.push_back(yepqhai.top().second);
+            yepqhai.pop();
         }
-        sort(uttar.begin(),uttar.end());
-        for(int i=0;i<k;i++){
-            vic[i]=uttar[i].second;
-        }
-        return vic;
-
+         return yhiuttarhai;
     }
 };
